a96ea8b57e44535627446377d03a31530a620a44
雾海 <nebulight123@outlook.com>
2025-11-25 13:56:50 +0800

    Add files via upload

---
+++ firefox.sh
@@ -0,0 +1,18 @@
+sudo echo AwA
+sudo apt update
+sudo apt install -y curl moby-engine moby-cli moby-containerd
+sudo docker version
+sudo systemctl enable --now docker
+sudo mkdir -p /data/firefox/config
+sudo mkdir -p /home/codespace/awa
+sudo chmod 777 -R ~/awa
+sudo docker run -d --name firefox \
+-e TZ=America/New_York \
+-e DISPLAY_WIDTH=1680 \
+-e DISPLAY_HEIGHT=1050 \
+-e KEEP_APP_RUNNING=1 \
+-e ENABLE_CJK_FONT=1 \
+-p 5800:5800 \
+-v /data/firefox/config:/config:rw \
+-v /home/codespace/awa:/home/abc/awa:rw \
+--shm-size 2g jlesage/firefox
